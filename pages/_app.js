fadaeeb40350387390dfbfc8b9338d7927f62102
JoBaHP <jocoraos@yahoo.com>
2022-07-22 18:33:32 +0200

    ga fix

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
+        id="ga-script"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script id="ga-datalayer" strategy="lazyOnload">
         {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
