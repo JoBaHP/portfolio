977e1047bfc46f3b7d100d383e2c018b67f18cd1
JoBaHP <55216493+JoBaHP@users.noreply.github.com>
2025-12-31 10:10:32 +0100

    fix: rename GA env var and install typescript

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Script from "next/script";
 import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
-  const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || "";
+  const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "";
   // If the ID doesn't start with 'G-' or 'UA-', assume it's base64 encoded to bypass secret scanners
   const finalId =
     gaId.startsWith("G-") || gaId.startsWith("UA-")
